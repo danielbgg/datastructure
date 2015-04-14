c48f425b82d71b92d46ecc8c1dab910c115a8981
kerdlix <danielbgg@gmail.com>
2015-04-14 12:58:23 -0300

    Maven

--- src/book1/chapter4/linkedlist/NewNode.java
+++
@@ -1,9 +0,0 @@
-package book1.chapter4.linkedlist;
-
-public class NewNode {
-	Listing2 l; // package access data members
-	NewNode next;
-
-	public NewNode() {
-	}
-}
--- src/book1/chapter6/recursion/MainFactorial.java
+++
@@ -1,8 +0,0 @@
-package book1.chapter6.recursion;
-
-public class MainFactorial {
-	public static void main(String[] args) {
-		long n = 4;
-		System.out.println(n + " factorial is " + Factorial.nFactorial(n));
-	}
-}
